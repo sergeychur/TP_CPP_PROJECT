57fd67a7030ab20e81b0ec8a8c69778f30bd0c71
sergeychur <churicovser@mail.ru>
2018-12-01 15:17:23 +0300

    renamed shared headers and srcs to shared dir

---
+++ shared_files/include/DefaultAbstractFactory.h
@@ -0,0 +1,54 @@
+//
+// Created by alex on 23.11.18.
+//
+
+#ifndef NET_DEFAULTABSTRACTFACTORY_H
+#define NET_DEFAULTABSTRACTFACTORY_H
+
+#include "include/Serializable.h"
+
+#include "update.hpp"
+#include "command.hpp"
+#include "initialiser.hpp"
+
+
+
+class DefaultAbstractFactory
+{
+public:
+	virtual std::shared_ptr<Serializable> create() = 0;
+	virtual ~DefaultAbstractFactory() = default;
+};
+
+class UpdateFactory : public DefaultAbstractFactory
+{
+public:
+	std::shared_ptr<Serializable> create() override
+	{
+		return std::static_pointer_cast<Serializable>(std::make_shared<Update>());
+	}
+	~UpdateFactory() override = default;
+};
+
+class CommandFactory : public DefaultAbstractFactory
+{
+public:
+	std::shared_ptr<Serializable> create() override
+	{
+		return std::static_pointer_cast<Serializable>(std::make_shared<Command>());
+	}
+    ~CommandFactory() override = default;
+};
+
+class InitialiserFactory : public DefaultAbstractFactory
+{
+public:
+    std::shared_ptr<Serializable> create() override
+    {
+        return std::static_pointer_cast<Serializable>(std::make_shared<Initialiser>());
+    }
+    ~InitialiserFactory() override = default;
+};
+
+
+#endif //NET_DEFAULTABSTRACTFACTORY_H
