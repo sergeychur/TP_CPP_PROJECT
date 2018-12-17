031d5f150a4ad08ee5f0778eb031c83085372dfc
sergeychur <churicovser@mail.ru>
2018-12-17 04:37:59 +0300

    chenged main a bit with linters

--- server_side/server/main.cpp
+++ server_side/server/main.cpp
@@ -7,83 +7,74 @@
 #include "initialiser.hpp"
 #include "adapter.hpp"
 
-// #include "ServerNetObject.h"
-
-
 
 int main() {
-    size_t player_num = 0;
-    Manager manager;
-    input(&player_num, "Enter the number of players");
-    Game game(player_num);
-    std::vector<std::pair<int, int>> bases;
-    try {
-        manager.read_bases_from_file(bases, player_num);
-    } catch(ManagerException& e) {
-        std::cout << e.what() << std::endl;
-        return ERR_READ;
-    }
-    uint port = 0;
-    input(&port, "Enter the port to listen on");
-    std::string ip;
-    input(&ip, "Enter the ip to listen on");
-    std::map<std::string, DefaultAbstractFactory*> map;
-    map = manager.get_instance_map();
-    std::cout << "Success entering" << std::endl;
-    // ServerNetObject server_obj(port, ip, player_num, map);
-    std::unique_ptr<Server> server = std::make_unique<Adapter>(port, ip, player_num, map);
-    server->work();
-    std::cout << "Success" << std::endl;
-    for(size_t i = 0; i < player_num; ++i) {
-        try {
-            game.add_player(bases[i], i);
-        } catch(std::invalid_argument& e) {
-            std::cerr << "Cannot add player, because of" << e.what() << std::endl;
-            throw e;
-        }
-        Initialiser init(i, player_num, bases);
-        try {
-             server->send_to(&init, i);
-        } catch(std::exception& e) {
-            std::cerr << "Cannot send cause of" << e.what() << std::endl;
-            return ERR_SEND;
-        }
-    }
-    size_t winner = player_num;
-    while(!game.is_win()) {
-		/*std::vector<std::shared_ptr<Serializable>> clients_data;
-        do {
-          clients_data = server.receive();
-        } while(clients_data.empty());*/
-        std::vector<Command> commands;
+	size_t player_num = 0;
+	Manager manager;
+	input(&player_num, "Enter the number of players");
+	Game game(player_num);
+	std::vector<std::pair<int, int>> bases;
+	try {
+		manager.read_bases_from_file(bases, player_num);
+	} catch (ManagerException &e) {
+		std::cout << e.what() << std::endl;
+		return ERR_READ;
+	}
+	uint port = 0;
+	input(&port, "Enter the port to listen on");
+	std::string ip;
+	input(&ip, "Enter the ip to listen on");
+	std::map<std::string, DefaultAbstractFactory *> map;
+	map = manager.get_instance_map();
+	std::cout << "Success entering" << std::endl;
+	std::unique_ptr<Server> server = std::make_unique<Adapter>(port, ip, player_num, map);
+	server->work();
+	std::cout << "Success" << std::endl;
+	for (size_t i = 0; i < player_num; ++i) {
+		try {
+			game.add_player(bases[i], i);
+		} catch (std::invalid_argument &e) {
+			std::cerr << "Cannot add player, because of" << e.what() << std::endl;
+			throw e;
+		}
+		Initialiser init(i, player_num, bases);
+		try {
+			server->send_to(&init, i);
+		} catch (std::exception &e) {
+			std::cerr << "Cannot send cause of" << e.what() << std::endl;
+			return ERR_SEND;
+		}
+	}
+	size_t winner = player_num;
+	while (!game.is_win()) {
+		std::vector<Command> commands;
 		do {
 			commands = server->receive();
-		} while(commands.empty());
+		} while (commands.empty());
 
-        try {
-            winner = game.act(commands);
-        }
-        catch (std::exception& e) {
-            std::cerr << "Can't act because of "<< e.what() << std::endl;
-        }
-        std::unique_ptr<Update> update;
+		try {
+			winner = game.act(commands);
+		}
+		catch (std::exception &e) {
+			std::cerr << "Can't act because of " << e.what() << std::endl;
+		}
+		std::unique_ptr<Update> update;
+		try {
+			update = game.get_update();
+		} catch (std::exception &e) {
+			std::cerr << "Can't get update because of " << e.what() << std::endl;
+		}
+		if (update) {
+			std::cout << "Update to send is:" << std::endl;
+			std::cout << *(update) << std::endl;       // in order to test, remove
+
 			try {
-				update = game.get_update();
-			} catch(std::exception& e) {
-			 	std::cerr << "Can't get update because of " << e.what() << std::endl;
+				server->send(update.get());
+			} catch (std::exception &e) {
+				std::cerr << "Cannot send, cause of " << e.what() << std::endl;
 			}
-        if(update) {
-            std::cout << "Update to send is:" << std::endl;
-            std::cout << *(update) << std::endl;       // in order to test, remove
-
-            try {
-                server->send(update.get());
-            } catch(std::exception& e) {
-                std::cerr << "Cannot send, cause of " << e.what() << std::endl;
-            }
-        }
-    }
-    std::cout << "And the winner iiiiiiiis player nuuuuuuumberrrr " << winner << std::endl;
-    // here should be sending the result to clients and destroying the connection
-    return 0;
+		}
+	}
+	std::cout << "And the winner iiiiiiiis player nuuuuuuumberrrr " << winner << std::endl;
+	return 0;
 }
